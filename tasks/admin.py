ef83825c5c7958426fbdfa090d47c2d436df7d0b
kazem <k_jebraeeli@yahoo.com>
2024-01-13 22:08:51 +0330

    models and simple view completed

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from tasks.models import TodoItem, Category
 # Register your models here.
+
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_date', 'completed']
+
+admin.site.register(TodoItem, TodoItemAdmin)
+admin.site.register(Category)
--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class TodoItem(models.Model):
     title = models.CharField(max_length=100)
     detail = models.TextField(null=True, blank=True)
-    date = models.DateTimeField(auto_add_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.title
