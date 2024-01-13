73ceb4ad73b9155388c468cacff22c2d5aef1973
kazem <k_jebraeeli@yahoo.com>
2024-01-13 21:27:34 +0330

    base models created for tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    detail = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_add_now=True)
+    completed = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
