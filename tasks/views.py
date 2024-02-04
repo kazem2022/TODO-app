90e50e907fbbfa2a4297930ece81044d38037cdb
kazem <k_jebraeeli@yahoo.com>
2024-02-04 10:55:12 +0330

    add login required

--- tasks/views.py
+++ tasks/views.py
@@ -3,21 +3,22 @@ from django.shortcuts import render
 
 from tasks.forms import TaskForm
 from tasks.models import Category, TodoItem
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def todo_list(request):
     items = TodoItem.objects.all()
     categories = Category.objects.all()
     context = {"items": items, "categories": categories}
     return render(request, "tasks/tasks_list.html", context=context)
 
-
+@login_required
 def todo_detail(request, pk):
     item = TodoItem.objects.get(pk=pk)
     context = {"item": item}
     return render(request, "tasks/task_detail.html", context=context)
 
-
+@login_required
 def todo_create(request):
     if request.method == "POST":
         forms = TaskForm(request.POST)
