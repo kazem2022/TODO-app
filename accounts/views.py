4919094084058140493faa71657b25d0d340c04e
kazem <k_jebraeeli@yahoo.com>
2024-03-06 13:15:21 +0330

    login base is completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,46 +1,9 @@
-# from django.shortcuts import render
-# from django.contrib.auth.forms import AuthenticationForm
-# from django.views import View
-# from django.contrib.auth import authenticate, login, logout
-# from django.http import HttpResponseRedirect
-# from django.shortcuts import redirect
-# # Create your views here.
-# class LoginView(View):
-    
-    # def get(self, request):
-    #     if request.method == 'GET':
-    #         form = AuthenticationForm()
-    #         context = {"form" : form}
-    #         return render(request, "login.html", context=context)
-    #     if request.method == 'POST':
-    #         form = AuthenticationForm(request=request, data=request.POST)
-    #         if form.is_valid():
-    #             username = form.cleaned_data.get('username')
-    #             password = form.cleaned_data.get('password')
-    #             user = authenticate(username=username, password=password)
-    #             print('******************************','\n',user)
-    #             if user is not None:
-    #                 login(request, user)
-    #                 return redirect('tasks/')
-    #             else:
-    #                 HttpResponseRedirect('/')
-
-# class LogoutView(View):
-    
-#     def get(self, request):
-#         logout(request)                  
-#         return redirect("/accounts/login/")
- 
- 
- 
-#  ---------------------------------------------------AI APPROACH-----------------------------   # 
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.views import View
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
-from django.urls import reverse
 
 class LoginView(View):
     
@@ -58,7 +21,16 @@ class LoginView(View):
                 user = authenticate(username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    return reverse('tasks')  # Redirect to the appropriate URL after successful login
+                    return redirect('/tasks')  # Redirect to the appropriate URL after successful login
             return HttpResponseRedirect('/')  # Redirect back to the login page if authentication fails
         else:
-            return redirect("/")
+            return redirect("/tasks")
+        
+        
+class LogoutView(View):
+    
+    def get(self, request):
+        logout(request)                  
+        return redirect("home")
+ 
+ 
